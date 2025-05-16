5fad49d0431a7701151d3cb7a34501db7d7b9bf1
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-16 22:54:09 +0000

    modify navbar & add toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
-
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">STARWARS</span>
+        </Link>
+        <div className="ml-auto">
+          {/* <Link to="/"> */}
+          <div className="dropdown">
+            <button className="btn btn-primary dropdown-toggle" type="button">
+              Favorites 1
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li>
+                <a class="dropdown-item" href="#">
+                  Action
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  Another action
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/* </Link> */}
+        </div>
+      </div>
+    </nav>
+  );
 };
