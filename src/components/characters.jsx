7bddc534373a80291c6f5e4687620b1e8b5c808c
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-16 23:46:29 +0000

    begin Fetch Characters

--- src/components/characters.jsx
+++ src/components/characters.jsx
@@ -2,16 +2,23 @@ import { useEffect } from 'react';
 
 export const characters = () => {
   useEffect(() => {
-    const base_url = 'https://www.swapi.tech/api/';
-    const endPoint = 'characters/1/';
-    const url = base_url + endPoint;
-    console.log(url);
-    // use async to fetch data
-    // await the data
-    // put the data in a variable
-    // use the variable to display the data
-
-    document.title = 'Characters';
+    async function fetchCharacters() {
+      const base_url = 'https://www.swapi.tech/api/';
+      const endPoint = 'characters/1/';
+      const url = base_url + endPoint;
+      console.log(url);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const characters = await response.json();
+        console.log(characters);
+      } catch (error) {
+        console.error('Error fetching characters:', error);
+      }
+    }
   }, []);
+  //   document.title = 'Characters';
   return <div>characters</div>;
 };
