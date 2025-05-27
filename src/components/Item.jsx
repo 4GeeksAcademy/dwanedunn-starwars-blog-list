a004f948d842710856a38388705c9a7312d13fc5
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-27 20:06:21 +0000

    refactor card

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,9 +5,6 @@ export const Item = ({ imgURL, imgAlt, itemTitle, itemDesc }) => {
       <div className="card-body">
         <h5 className="card-title">{itemTitle}</h5>
         <p className="card-text">{itemDesc}</p>
-        <p className="card-text">
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </p>
       </div>
     </div>
   );
