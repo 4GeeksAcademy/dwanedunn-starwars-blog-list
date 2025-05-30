615d49c8134d7d7df7784c651a44f4763b6754f7
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-30 00:00:01 +0000

    updated urls

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
 export const initialStore = () => {
   return {
     // message: null,
-    PEOPLE_URL: 'https://www.swapi.tech/api/people?page=1&limit=8',
-    PLANETS_URL: 'https://www.swapi.tech/api/planets?page=1&limit=8',
-    VEHICLES_URL: 'https://www.swapi.tech/api/vehicles?page=1&limit=8',
+    PEOPLE_URL:
+      'https://www.swapi.tech/api/people?page=2&limit=8&expanded=true',
+    PLANETS_URL:
+      'https://www.swapi.tech/api/planets?page=1&limit=8&expanded=true',
+    VEHICLES_URL:
+      'https://www.swapi.tech/api/vehicles?page=1&limit=8&expanded=true',
     PEOPLE_IMGAES_URL:
       'https://raw.githubusercontent.com/breatheco-de/swapi-images/master/public/images/people',
     PLANETS_IMAGES_URL:
